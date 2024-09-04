46a668220d9193e2fd65e5201f284f8ed4e60528
FrancesC8 <frances.caban2@upr.edu>
2024-09-04 03:06:49 -0400

    Added initial model/handler/controller for users and connected MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import createMongoDbConnection from './database/MongoDbConnection.js';
+
 import HealthCheckController from './controllers/HealthCheckController.js';
+import UsersController from './controllers/UsersController.js';
+
+import UsersHandler from './handlers/UsersHandler.js';
 
 // Load environment variables
 dotenv.config();
 
+// Setup database connection
+createMongoDbConnection(process.env.MONGODB_CONNECTION_STRING);
+
 // Create an express application
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Register middlewares
 app.use(express.json());
 
+// Create Handlers
+const usersHandler = new UsersHandler();
+
 // Create Controllers
 const healthCheckController = new HealthCheckController();
+const usersController = new UsersController(usersHandler);
 
 // Register endpoints
-app.get('/healthz', healthCheckController.index);
+app.get('/healthz', async (req, res) => healthCheckController.index(req, res));
+app.get('/users', async (req, res) => usersController.getAllUsers(req, res));
+app.post('/users', async (req, res) => usersController.createUser(req, res));
 
 // Run server
 app.listen(port, () => {
---
+++ backend/src/models/UserModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+});
+
+export default mongoose.model('User', userSchema);
