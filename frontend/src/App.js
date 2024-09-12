c17b1207b8cae4e83a86871466471babb5b30773
ebrain-mendoza121 <ebrain.mendoza@upr.edu>
2024-09-12 18:51:37 -0400

    Simple lobby interface. Just showing on the App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Lobby from './lobby/lobby.js';
 import MainGame from './gamestate/experimentalGame.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MainGame/>
+        <Lobby/>
       </header>
     </div>
   );
