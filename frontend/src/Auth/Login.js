78e6f84112cc956f588a4b1a672f9f3b9f54ac57
Frances M. Caban Quiles <99444066+FrancesC8@users.noreply.github.com>
2024-10-07 14:20:09 -0400

    Added changes to deploy frontend and support for environment variables. (#207)

--- frontend/src/Auth/Login.js
+++ frontend/src/Auth/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import config from './config.js';
 import './Login.css';
 import GameMode from '../gameMode.js';
 
@@ -18,7 +17,7 @@ function Login() {
 
     try {
       console.log(JSON.stringify(payload));
-      const response = await fetch(`${config.backendUrl}${endpoint}`, {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}${endpoint}`, {
         headers: {
           'Content-Type': 'application/json',
         },
