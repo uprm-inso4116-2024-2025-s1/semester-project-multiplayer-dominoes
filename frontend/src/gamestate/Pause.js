883a8c6f6dfd0bf486899f7aef9183f09563bf92
Ian Gabriel <ian.ramos3@upr.edu>
2024-10-22 15:20:00 -0400

    Enhanced the Pause Menu

--- frontend/src/gamestate/Pause.js
+++ frontend/src/gamestate/Pause.js
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router-dom";
 
 export default function PauseScreen({ onResume }) {
     const navigate = useNavigate();
+    
     return (
-        <div><p>Game Paused</p>
-            <button onClick={onResume}>Resume Game</button>
-            <button onClick={() => {
-                navigate('/');
-            }}>Back to login</button>
-        </div >
-    )
+        <div className="overlay">
+            <img src={'gamePaused.png'} alt="Game Paused" className="pause-image" />
+            <div className="button-container">
+                <button onClick={onResume}>Resume Game</button>
+                <button onClick={() => navigate('/')}>Back To Login</button>
+            </div>
+        </div>
+    );
 }
