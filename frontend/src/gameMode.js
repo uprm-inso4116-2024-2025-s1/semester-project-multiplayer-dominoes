7b81fab86dc3e889f741a3e69ea5740e4e86310c
ebrain-mendoza121 <ebrain.mendoza@upr.edu>
2024-10-22 17:46:42 -0400

    Extended lobby ui to select between solo play or multiplayer. Includes game mode selection and difficulty selection for bots

--- frontend/src/gameMode.js
+++ frontend/src/gameMode.js
@@ -4,9 +4,9 @@ export default function GameMode() {
     const navigate = useNavigate();
     return (
         <div><p>Choose game mode:</p>
-            <button onClick={() => navigate('/game', { state: { gameMode: 'classic' } })}>vs. AI</button>
+            <button onClick={() => navigate('/game', { state: { gameMode: 'classic', bot: 'placeholder' } })}>vs. AI</button>
             <button onClick={() => {
-                navigate('/game', { state: { gameMode: 'allFives' } })
+                navigate('/game', { state: { gameMode: 'allFives', bot: 'placeholder' } })
             }}>All Fives</button>
         </div >
     )
