6a9c7b751b407218dd2b818d8d67f2ff9ce01f68
FrancesC8 <frances.caban2@upr.edu>
2024-10-23 00:10:36 -0700

    Moved logout function to user controller.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,20 +3,11 @@ import authenticateJWT from '../middleware/authenticateJWT.js'
 
 export default (container) => {
     const router = Router();
-    const usersHandler = container.UsersHandler;
     const usersController = container.UsersController;
 
     router.post('/register', async (req, res) => usersController.createUser(req, res));
     router.post('/login', async (req, res) => usersController.loginUser(req, res));
-    router.post('/logout', authenticateJWT, async (req, res) => {
-        try {
-            const userId = req.user.id;
-            await usersHandler.logoutUser(userId);
-            res.status(200).json({ message: 'Logged out successfully' });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    });
+    router.post('/logout', authenticateJWT, async (req, res) => usersController.logoutUser(req, res));
 
     return router;
 };
