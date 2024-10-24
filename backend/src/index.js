fe85727222606bb68be53b3b51934238f7ce27a6
AnthonyMed13 <anthony.medina6@upr.edu>
2024-10-24 13:30:15 -0400

    update the index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
-import createMongoDbConnection from './database/MongoDbConnection.js';
-import HealthCheckController from './controllers/HealthCheckController.js';
-import UsersController from './controllers/UsersController.js';
 import container from './di/container.js';
 import routes from './routes/index.js';
 import MongoDbConnection from './database/MongoDbConnection.js';
 
 // Load environment variables
 dotenv.config();
-
-import authenticateJWT from './middleware/authenticateJWT.js';
-import UsersHandler from './handlers/UsersHandler.js';
-
 export const JWT_SECRET = process.env.JWT_SECRET || 'default_secret_key';
-// Setup database connection
-createMongoDbConnection(process.env.MONGODB_URI);
-
 // Create an express application
 const app = express();
 const port = process.env.PORT || 8080;
@@ -29,14 +18,6 @@ MongoDbConnection.createConnection(process.env.MONGODB_URI);
 // Register middlewares
 app.use(cors());
 app.use(express.json());
-
-
-// Create Handlers
-const usersHandler = new UsersHandler();
-
-// Create Controllers
-const healthCheckController = new HealthCheckController();
-const usersController = new UsersController(usersHandler);
 
 // Register endpoints
 routes(app, container);
