a88574808d5219b254eb416ef625b8e655de80a3
Gabriel  Viera <gabriel.viera4@upr.edu>
2024-11-23 01:07:13 -0400

    Update table.js

--- frontend/src/gamestate/table.js
+++ frontend/src/gamestate/table.js
@@ -257,7 +257,6 @@ class Table{
                                     current_domino.flipped = true;
                             }
                         }else if(adjacent_domino.displayDirection === DisplayDirection.VERTICAL){
-                            console.log("DISPLAY VERTICAL",adjacent_domino.values,adjacent_domino.freeCorners,current_domino.values)
                             if ((new_coords[0]-1 >= 0
                                 && this.#data_matrix[new_coords[0]-1][new_coords[1]] != null
                                 && adjacent_domino.values[1] === current_domino.values[1]) 
@@ -266,7 +265,6 @@ class Table{
                                     current_domino.values[0] = current_domino.values[1];
                                     current_domino.values[1] = temp;
                                     current_domino.flipped = true;
-                                    console.log("AQUI  IZQUIERDA",adjacent_domino.values,adjacent_domino.freeCorners,current_domino.values)
                             }
                             else if ((new_coords[0]+1 < this.#data_matrix.length 
                                 && this.#data_matrix[new_coords[0]+1][new_coords[1]] != null
@@ -275,8 +273,6 @@ class Table{
                                     current_domino.values[0] = current_domino.values[1];
                                     current_domino.values[1] = temp;
                                     current_domino.flipped = true;
-                                    console.log("AQUI ESTOY BREGANDO DERECHA",adjacent_domino.values,adjacent_domino.freeCorners,current_domino.values)
-
                             }
                         }
 
