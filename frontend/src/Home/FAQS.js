01f5e97cb59c2f4f62ecf37009a6265fba19944c
Ian Gabriel <94259496+igrv2003@users.noreply.github.com>
2024-11-25 15:18:30 -0800

    Add “Back To Home” Button to Contact Us Page (#409)
    * Add "Back To Home" button to Contact Us page with styling and routing
    
    * Added ‘Back to Home’ button to FAQs and Privacy Policy pages

--- frontend/src/Home/FAQS.js
+++ frontend/src/Home/FAQS.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import './Home.css'; 
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 const FAQS = () => (
-    <div>
-      <h1>Frequently Asked Questions</h1>
-      <br></br>
-      <h3>1. How do I start a game?</h3>
-      <p>To start a game, log in, go to the lobby, and join or create a game room. You can play with friends or against bots.</p>
-      <br></br>
-      <h3>2. What are the rules of Dominoes?</h3>
-      <p>The goal of Dominoes is to be the first player to play all of your tiles or have the lowest score when no moves are possible.</p>
-      <br></br>
-      <h3>3. Can I play with my friends?</h3>
-      <p>Yes, you can invite friends to join a game room with you, or you can play solo against computer players.</p>
-      <br></br>
-      <h3>4. How do I report a problem?</h3>
-      <p>If you encounter any issues, please visit our <a href="/contact-us">Contact Us</a> page to get in touch.</p>
-    </div>
-  );
-  
-  export default FAQS;
+  <div>
+    <h1>Frequently Asked Questions</h1>
+    <br />
+    <h3>1. How do I start a game?</h3>
+    <p>
+      To start a game, log in, go to the lobby, and join or create a game room.
+      You can play with friends or against bots.
+    </p>
+    <br />
+    <h3>2. What are the rules of Dominoes?</h3>
+    <p>
+      The goal of Dominoes is to be the first player to play all of your tiles
+      or have the lowest score when no moves are possible.
+    </p>
+    <br />
+    <h3>3. Can I play with my friends?</h3>
+    <p>
+      Yes, you can invite friends to join a game room with you, or you can play
+      solo against computer players.
+    </p>
+    <br />
+    <h3>4. How do I report a problem?</h3>
+    <p>
+      If you encounter any issues, please visit our{" "}
+      <a href="/contact-us">Contact Us</a> page to get in touch.
+    </p>
+    <br />
+    <Link to="/" className="back-to-home-btn">
+      Back To Home
+    </Link>
+  </div>
+);
+
+export default FAQS;
