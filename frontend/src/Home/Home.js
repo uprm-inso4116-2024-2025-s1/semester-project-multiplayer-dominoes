a508121874ae63e91671e6d698c73f705e2a8f4d
JadiellMV <jadiell.martir@upr.edu>
2024-11-26 22:11:32 -0400

    Styling fixed

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -17,15 +17,15 @@ const Home = () => {
     const handleProfileClick = () => {
         navigate('/profile'); // Navigate to profile page
     };
-  const toggleRules = () => {
-      setShowRules(!showRules);
+    const toggleRules = () => {
+        setShowRules(!showRules);
     };
-  
+
     const playSound = () => {
-      const audio = document.getElementById('howToPlaySound');
-      if (audio) {
-        audio.play();
-      }
+        const audio = document.getElementById('howToPlaySound');
+        if (audio) {
+            audio.play();
+        }
     };
 
     return (
@@ -39,30 +39,30 @@ const Home = () => {
                 <nav>
                     <div className="footer-links">
                         <ul>
-                            <a onClick={()=>playSound()} className="instruction-link" href="/">Home</a>
-                            <a onClick={()=>playSound()} className="instruction-link" href="#">Instructions</a>
-                            <a onClick={()=>playSound()} className="instruction-link" href="#">FAQs</a>
+                            <a onClick={() => playSound()} className="instruction-link" href="/">Home</a>
+                            <a onClick={() => playSound()} className="instruction-link" href="#">Instructions</a>
+                            <a onClick={() => playSound()} className="instruction-link" href="#">FAQs</a>
                         </ul>
                     </div>
                 </nav>
                 <div className="footer-links">
                     {user.username ? (
                         <>
-                            {/* Profile Picture Button */}
-                            <button className="profile-button" onClick={handleProfileClick}>
-                                <img
-                                    src={user.profilePicture}
-                                    alt="User Profile"
-                                    className="profile-picture"
-                                />
-                            </button>
-                            {/* Play Button */}
-                            <button className="play-button" onClick={() => navigate('/lobby')}>
-                                Play
-                            </button>
+                            <div className="profile-container">
+                                <button className="profile-button" onClick={handleProfileClick}>
+                                    <img
+                                        src={user.profilePicture}
+                                        alt="User Profile"
+                                        className="profile-picture"
+                                    />
+                                </button>
+                                <button className="play-button" onClick={() => navigate('/lobby')}>
+                                    Play
+                                </button>
+                            </div>
                         </>
                     ) : (
-                        <a onClick={()=> playSound()} className="instruction-link" href="/login">Sign Up / Log In</a>
+                        <a onClick={() => playSound()} className="instruction-link" href="/login">Sign Up / Log In</a>
                     )}
                 </div>
             </header>
@@ -73,27 +73,27 @@ const Home = () => {
             </div>
 
             <section className="instructions">
-            <h2>INSTRUCTIONS</h2>
-            <p>Dominoes is a tile game where the objective is to be the first to get rid of all your tiles or have the lowest score at the end. 
-                Players take turns placing tiles, connecting matching numbers at the ends of the chain of tiles on the table. 
-                If a player cannot make a move, they must skip their turn. 
-                The game continues until a player runs out of tiles or no more moves are possible. 
-                The winner scores points based on the tiles remaining in the opponents' hands, and the first player to reach a predetermined number of points wins the game.</p>
-            <a onClick={() => { toggleRules(); playSound();}} className="instruction-link">Click here for more in-depth instructions</a>
-            {showRules && (
-                  <div className="rules-overlay">
-                    <div className="rules">
-                      <h2>How to Play Dominoes</h2>
-                      <p><strong>Players:</strong> Two, three, or four may play. If four are playing, it may be played as a partnership (the two players sitting opposite one another are partners).</p>
-                      <p><strong>The Shuffle:</strong> The dominoes are shuffled. Players draw one domino. The player drawing the highest double, or if no double, the highest domino plays first. Re-shuffle and begin drawing the first hand.</p>
-                      <p><strong>Drawing:</strong> Each player draws seven dominoes for their hand. The remaining dominoes (the boneyard) are left face down to be drawn later if a player is unable to play.</p>
-                      <p><strong>Begin Play:</strong> The player who drew the highest domino plays first. The objective is to score points by laying dominoes end to end. If the dots on the exposed ends total a multiple of five, the player is awarded points.</p>
-                      <p><strong>Blocking the Game:</strong> If no player can play, the game is blocked. The player with the least spots in their hand scores the total spots of the opponents' hands.</p>
+                <h2>INSTRUCTIONS</h2>
+                <p>Dominoes is a tile game where the objective is to be the first to get rid of all your tiles or have the lowest score at the end.
+                    Players take turns placing tiles, connecting matching numbers at the ends of the chain of tiles on the table.
+                    If a player cannot make a move, they must skip their turn.
+                    The game continues until a player runs out of tiles or no more moves are possible.
+                    The winner scores points based on the tiles remaining in the opponents' hands, and the first player to reach a predetermined number of points wins the game.</p>
+                <a onClick={() => { toggleRules(); playSound(); }} className="instruction-link">Click here for more in-depth instructions</a>
+                {showRules && (
+                    <div className="rules-overlay">
+                        <div className="rules">
+                            <h2>How to Play Dominoes</h2>
+                            <p><strong>Players:</strong> Two, three, or four may play. If four are playing, it may be played as a partnership (the two players sitting opposite one another are partners).</p>
+                            <p><strong>The Shuffle:</strong> The dominoes are shuffled. Players draw one domino. The player drawing the highest double, or if no double, the highest domino plays first. Re-shuffle and begin drawing the first hand.</p>
+                            <p><strong>Drawing:</strong> Each player draws seven dominoes for their hand. The remaining dominoes (the boneyard) are left face down to be drawn later if a player is unable to play.</p>
+                            <p><strong>Begin Play:</strong> The player who drew the highest domino plays first. The objective is to score points by laying dominoes end to end. If the dots on the exposed ends total a multiple of five, the player is awarded points.</p>
+                            <p><strong>Blocking the Game:</strong> If no player can play, the game is blocked. The player with the least spots in their hand scores the total spots of the opponents' hands.</p>
+                        </div>
+                        <button button onClick={() => { toggleRules(); playSound(); }} className="close-rules-button">Close Instructions</button>
                     </div>
-                    <button button onClick={() => { toggleRules(); playSound(); }} className="close-rules-button">Close Instructions</button>
-                  </div>
                 )}
-        </section>
+            </section>
 
 
             {/* Leaderboard and Reviews Section */}
