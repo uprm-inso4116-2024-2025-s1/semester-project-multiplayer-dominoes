aa133588cabfe5d4765fbc4b373b69c8f101b0d7
FrancesC8 <frances.caban2@upr.edu>
2024-11-26 23:41:08 -0800

    Created and modified new models.

--- backend/src/models/UserAchievementModel.js
+++ backend/src/models/UserAchievementModel.js
@@ -1,24 +1,22 @@
 import mongoose from "mongoose";
 
-const matchSchema = new mongoose.Schema({
-    roomId: { 
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Room',
-    },
-    winnerId: {
+const userAchievementSchema = new mongoose.Schema({
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+    },
+    achievementId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Achievement',
     },
     status: {
         type: String,
         enum: ['Not Started', 'In Progress', 'Completed'],
         default: 'Not Started',
     },
-}, {
-    timestamps: { 
-        createdAt: 'startTime', 
-        updatedAt: 'endTime',
+    earnedAt: {
+        type: Date,
     },
 });
 
-export default mongoose.model('Match', matchSchema);
+export default mongoose.model('UserAchievement', userAchievementSchema);
